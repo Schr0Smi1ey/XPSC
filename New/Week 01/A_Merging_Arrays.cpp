a5eb80b76d370e442036fa9ce6fc8d9c35de2029
Schrosmiley <radiantremel444@gmail.com>
2024-01-03 23:58:23 +0600

    03.01.23

---
+++ New/Week 01/A_Merging_Arrays.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define fastio ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define endl "\n"
+#define int long long int
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
+        tree_order_statistics_node_update>;
+#ifdef KARIM
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1e9 + 7;
+const int INF = 1e18, MINF = -1e18;
+const int N = 2e5 + 5;
+
+void solution() {
+    int n,m;cin >> n >> m;
+    int a[n],b[m];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    for(int i=0;i<m;i++){
+        cin >> b[i];
+    }
+    int ans[n+m],i = 0,j = 0,k = 0;
+    while(i < n && j < m){
+        if(a[i] > b[j]){
+            ans[k++] = b[j++];
+        }
+        else{
+            ans[k++] = a[i++];
+        }
+    }
+    while(i < n){
+        ans[k++] = a[i++];
+    }
+    while(j < m){
+        ans[k++] = b[j++];
+    }
+    for(auto x : ans){
+        cout << x << " ";
+    }
+}
+
+int32_t main() {
+    #ifndef KARIM
+    fastio;
+    #endif
+    int t = 1;
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
