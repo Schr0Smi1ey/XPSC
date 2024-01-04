b47e3d7aee0de97464b00392f9f880f1163359ed
Schrosmiley <radiantremel444@gmail.com>
2024-01-04 21:16:50 +0600

    04.01.23

---
+++ New/Week 01/B_Number_of_Smaller.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define fastio ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define endl "\n"
+#define int long long int
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
+        tree_order_statistics_node_update>;
+#ifdef KARIM
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1e9 + 7;
+const int INF = 1e18, MINF = -1e18;
+const int N = 2e5 + 5;
+int a[N],b[N],n,m;
+int order(int target){
+    int low = 0,high = n-1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(a[mid] < target){
+            low = mid + 1;
+        }
+        else{
+            high = mid -1;
+        }
+    }
+    return low;
+}
+void solution() {
+    cin >> n >> m;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    for(int i=0;i<m;i++){
+        cin >> b[i];
+        cout << order(b[i]) << " ";
+    }
+}
+
+int32_t main() {
+    #ifndef KARIM
+    fastio;
+    #endif
+    int t = 1;
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
