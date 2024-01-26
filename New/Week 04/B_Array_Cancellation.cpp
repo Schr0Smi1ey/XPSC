d10a63ce4c86a714188e0e5c9f35d8a95da4ffa9
Schrosmiley <radiantremel444@gmail.com>
2024-01-26 20:58:55 +0600

    26.01.23

---
+++ New/Week 04/B_Array_Cancellation.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define fastio ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define endl "\n"
+#define int long long int
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
+        tree_order_statistics_node_update>;
+#ifdef KARIM
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1e9 + 7;
+const int INF = 1e18, MINF = -1e18;
+const int N = 2e5 + 5;
+
+void solution() {
+    int n;cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    bool flag = false;
+    int ans = 0LL,plus = 0LL,minus = 0LL;
+    for(int i=0;i<n;i++){
+        if(arr[i] > 0)flag = true;
+        if(flag){
+            if(arr[i] > 0) {
+                ans += abs(min(0LL,plus + minus));
+                plus = (plus + minus > 0)?(plus + minus + arr[i]):arr[i];
+                minus = 0LL;
+            }
+            else{
+                minus += arr[i];
+            }
+        }
+        else{
+            ans += abs(arr[i]);
+        }
+    }
+    ans += abs(min(0LL,plus + minus));
+    cout << ans << endl;
+}
+
+int32_t main() {
+    #ifndef KARIM
+    fastio;
+    #endif
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
